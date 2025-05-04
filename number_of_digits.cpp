41871d6164e38dee967c5d8793d7ce997b0d5f51
palakbansal456 <bansalpalak039@gmail.com>
2025-05-04 15:44:54 +0530

    Add files via upload

---
+++ number_of_digits.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n, count = 0;
+    cout<<"Enter a number: ";
+    cin>>n;
+    if(n == 0){
+        count = 1;
+    }else{
+        if(n < 0){
+            n = -n;
+        }while(n != 0){
+            n = n / 10;
+            count++;
+        }
+    }
+    cout<<"Number of digits: "<<count<<endl;
+    return 0;
+}
