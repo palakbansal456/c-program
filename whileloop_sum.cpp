e2d62712406cc14dd179cdd879adf2744c9658ca
palakbansal456 <bansalpalak039@gmail.com>
2025-05-04 14:56:18 +0530

    Add files via upload

---
+++ whileloop_sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=1;
+    int sum;
+    while (i<=50)
+    {
+        sum += i;
+        i++;
+    }
+    cout<<"The sum of first 50 natural number is: "<<sum<<endl;
+    
+}
