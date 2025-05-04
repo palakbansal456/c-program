482acc853c6668e02a9e43aa8f5b1d0af06930d1
palakbansal456 <bansalpalak039@gmail.com>
2025-05-04 16:26:12 +0530

    Add files via upload

---
+++ gdc_of_two_no.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 48;
+    int b = 18;
+
+    // int originalA = a, originalB = b;
+
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    cout << "The GCD of given number is: " << a << endl;
+
+    return 0;
+}
