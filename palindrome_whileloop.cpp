c3a228b68f6c27d62ea94dde4abb5fa9441609ff
palakbansal456 <bansalpalak039@gmail.com>
2025-05-05 03:02:39 +0530

    Create palindrome_whileloop.cpp

---
+++ palindrome_whileloop.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number, reversed = 0, remainder, original;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    original = number;
+
+    while (number != 0) {
+        remainder = number % 10;
+        reversed = reversed * 10 + remainder;
+        number /= 10;
+    }
+
+    if (original == reversed) {
+        cout << original << " is a palindrome." << endl;
+    } else {
+        cout << original << " is not a palindrome." << endl;
+    }
+
+    return 0;
+}
