b69f4488624e4dc6d8081614efa2795c1457c5ee
palakbansal456 <bansalpalak039@gmail.com>
2025-05-06 21:20:59 +0530

    Create cube_inline_function.cpp

---
+++ cube_inline_function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+inline int cube(int n) {
+    return n * n * n;
+}
+
+int main() {
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "Cube of " << num << " is: " << cube(num) << endl;
+
+    return 0;
+}
