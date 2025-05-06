bf960f5f403fdd404ce16785ec6afae7544ff895
palakbansal456 <bansalpalak039@gmail.com>
2025-05-06 21:55:21 +0530

    Create default_argument1.cpp

---
+++ default_argument1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void greet(string name = "Guest") {
+    cout << "Hello, " << name << "!" << endl;
+}
+
+int main() {
+    greet();
+    greet("Alice");
+    greet("John Doe");
+
+    return 0;
+}
